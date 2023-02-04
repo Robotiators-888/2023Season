483d6bbab63989b63b5a880bab1065adb1bd9ff6
davidcaptilist <yohahaha1234567@gmail.com>
2023-02-04 14:55:13 -0500

    changed from using Y value to stop to using the distance

--- src/main/java/frc/robot/commands/CMD_LimeDrive.java
+++ src/main/java/frc/robot/commands/CMD_LimeDrive.java
@@ -23,22 +23,23 @@ public class CMD_LimeDrive extends CommandBase {
     public void execute() {
         // We are using the Y value, because the Y value will change as you get closer or further
         // We got a pretty accurate value for what the y value should be in order for us to get as close as we can
-        double distance = limelight.getY();
+        double distance = limelight.getDistance();
 
         // We check if we can even find one to begin with, or it will be impossible to turn to something tha tdoesn't exist
         if(limelight.getTv()){
             // We found that -3.6 is the correct number for Y, so as long as we don't reach it, we will drive forward
-            if (distance > -3.6) {
+            if (distance > 40) {
                 drive.setMotorsArcade( 0.4, 0.0);
                 System.out.println(distance);
             }
         }
 
         SmartDashboard.putBoolean("LimeDriveStart", true);
+        System.out.println("LimeDrive: True");
     }
     public boolean isFinished(){
         // If we reach the place, then we stop
-        if (limelight.getY() <= -3.6){
+        if (limelight.getDistance() <= 40){
             return true;
         }else{
             return false;
