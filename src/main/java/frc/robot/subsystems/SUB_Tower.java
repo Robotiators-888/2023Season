39255e0c20a74a9caba3e535aa467b8f8e9a422f
Kenneth Wang <109991402+kennlw@users.noreply.github.com>
2023-02-09 17:02:33 -0500

    set current limits

--- src/main/java/frc/robot/subsystems/SUB_Tower.java
+++ src/main/java/frc/robot/subsystems/SUB_Tower.java
@@ -8,28 +8,33 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
 public class SUB_Tower extends SubsystemBase {
+    //set motors
     public CANSparkMax towerMotor = new CANSparkMax(Constants.TOWER_SPARKMAX_CAN_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    //declare encoders
     public SparkMaxAbsoluteEncoder.Type m_encoder = SparkMaxAbsoluteEncoder.Type.kDutyCycle;
     public AbsoluteEncoder throughBoreEncoder = towerMotor.getAbsoluteEncoder(m_encoder);
 
-    //setpoint is arbitrary
+    //declares and sets PID, setpoint is arbitrary
     PIDController pid = new PIDController(Constants.PID_kP, Constants.PID_kI, Constants.PID_kD);
     double setpoint = 42.0;
     //Counteract Gravity on Arm, Currently lbsArm is arbitrary (For kG of FF)
     double lbsArm = 30;
     double gravitional_force_in_Kg = (lbsArm*4.44822162)/9.8;
     // Create a new ElevatorFeedforward with gains kS, kG, kV, and kA
-    ElevatorFeedforward feedforward = new ElevatorFeedforward(Constants.FF_kS, Constants.FF_kG, Constants.FF_kV, Constants.FF_kA);
+    ArmFeedforward feedforward = new ArmFeedforward(Constants.FF_kS, Constants.FF_kG, Constants.FF_kV, Constants.FF_kA);
     
 
     public void setLimits(){
+        //set soft limits and current limits for how far the manip can move
+        towerMotor.setSmartCurrentLimit(0, 20);
+
         towerMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
         towerMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
 
-        towerMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 128);
+        towerMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 25);
         towerMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 0);
     }
 
@@ -39,6 +44,7 @@ public class SUB_Tower extends SubsystemBase {
     }
 
     public double getRotations(){
+        //gets position
         return throughBoreEncoder.getPosition();
     }
 }
