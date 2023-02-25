8360612b194dc042b4e1e48e7b2ab52c95381be4
Spencer McGrath <Samcgrath2005@gmail.com>
2023-02-24 21:54:09 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
     public static final double kTurningScale = -0.5;
   }
     public static final double kTurningScale = -0.5;
-}
+
 
 //Joystick Constants
  public static final int JOYSTICK_PORT = 0;
