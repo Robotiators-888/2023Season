ca3b0cbd2baeafc1b051ef2a52b6a1afd14ba1f4
organelleV <fitchw42@gmail.com>
2023-02-26 11:45:29 -0500

    main pulled into LED

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,9 @@ import edu.wpi.first.cscore.VideoMode;
 import edu.wpi.first.math.MathUtil;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -20,10 +23,10 @@ import frc.robot.commands.*;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.subsystems.blinkin;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 
 /**
@@ -35,21 +38,64 @@ import edu.wpi.first.wpilibj.DataLogManager;
 public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
-  private final SUB_Gripper gripper = new SUB_Gripper();
-  private final SUB_Drivetrain drivetrain = new SUB_Drivetrain();
-  private final SUB_Tower tower = new SUB_Tower();
-  private Joystick controller = new Joystick(Constants.JOYSTICK_PORT);
 
-  JoystickButton rBumper = new JoystickButton(controller, 5);
-  JoystickButton lBumper = new JoystickButton(controller, 6);
+  //TODO: Adjust buttons and button numbers as needed
 
+  public static final Field2d field2d = new Field2d();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+  public static final SUB_Gripper gripper = new SUB_Gripper();
+  public static final SUB_Drivetrain drivetrain = new SUB_Drivetrain(field2d);
+  public static final SUB_Tower tower = new SUB_Tower();
+  public static SUB_Limelight limelight = new SUB_Limelight();
+  public static SUB_AprilTag apriltag = new SUB_AprilTag();
+  public static CMD_LimeSequential LimeSequential = new CMD_LimeSequential();
+  public static CMD_AprilSequential AprilSequential = new CMD_AprilSequential();
+  private static final Autonomous autos = new Autonomous();
+  public final static blinkin m_blinkin = new blinkin(Constants.KBLINKIN);
   
+  private final Joystick controller = new Joystick(Constants.JOYSTICK_PORT);
+  private final Joystick controller2 = new Joystick(Constants.JOYSTICK_PORT2);
+
+  private JoystickButton c_rBumper = new JoystickButton(controller2, 5);
+  private JoystickButton c_lBumper = new JoystickButton(controller2, 6);
+  private JoystickButton c_aButton = new JoystickButton(controller2, 1);
+  private JoystickButton c_bButton = new JoystickButton(controller2, 2);
+  private JoystickButton c_yButton = new JoystickButton(controller2, 4);
+  private JoystickButton c_xButton = new JoystickButton(controller2, 3);
+
+  JoystickButton c0_yButton = new JoystickButton(controller, 4);
+  JoystickButton c0_bButton = new JoystickButton(controller, 2);
+  JoystickButton c0_xButton = new JoystickButton(controller, 3);
+  JoystickButton c0_aButton = new JoystickButton(controller, 1);
+
+ // Auto objects
+ SendableChooser<Command> AutoChooser = new SendableChooser<>();
+ SendableChooser<Integer> DelayChooser = new SendableChooser<>();
+
+ 
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    
-    // Configure the trigger bindings
+    CameraServer.startAutomaticCapture(0)
+    .setVideoMode(new VideoMode(VideoMode.PixelFormat.kMJPEG, 416, 240, 180));
+
+    AutoChooser.setDefaultOption("Red 1 - One Game Piece", autos.red1_1GP);
+
+    DelayChooser.setDefaultOption("0 sec", 0);
+    DelayChooser.addOption("1 sec", 1);
+    DelayChooser.addOption("2 sec", 2);
+    DelayChooser.addOption("3 sec", 3);
+    DelayChooser.addOption("4 sec", 4);
+    DelayChooser.addOption("5 sec", 5);
+    DelayChooser.addOption("6 sec", 6);
+    DelayChooser.addOption("7 sec", 7);
+    DelayChooser.addOption("8 sec", 8);
+    DelayChooser.addOption("9 sec", 9);
+    DelayChooser.addOption("10 sec", 10);
+
+    SmartDashboard.putData("Auto Chooser", AutoChooser);
+    SmartDashboard.putData("Delay Chooser", DelayChooser);
+
 
 
     configureBindings();
@@ -69,23 +115,95 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    limelight.setDefaultCommand(new InstantCommand(() -> limelight.setLed(1), limelight));
+    // Press the Y button once, then we will start the sequence and press it again we stop
+    // Press the B button once, then the april tag sequence will start
+    c0_yButton.onTrue(LimeSequential);
+    c0_bButton.onTrue(AprilSequential);
+    
+    
+    c_lBumper
+    .onTrue(new InstantCommand(() -> {gripper.openConeGripper();SmartDashboard.putNumber("Gripper Status", gripper.getSetPosition());}))
+    .onFalse(new InstantCommand(() -> {gripper.closeConeGripper();SmartDashboard.putNumber("Gripper Status", gripper.getSetPosition());}));
+    //.onFalse(new InstantCommand(() -> {m_gripper.driveGripper(-0.25);SmartDashboard.putNumber("Gripper Status", m_gripper.getSetPosition());}));
+    
+    /* 
+    c_rBumper
+    .onTrue(new RunCommand(()-> {gripper.openCubeGripper();}, gripper))
+    .onFalse(new RunCommand(()->{gripper.closeCubeGripper();}, gripper));
+    */
 
-    drivetrain.setDefaultCommand(new RunCommand( ()-> drivetrain.setMotorsArcade(controller.getRawAxis(Constants.LEFT_AXIS), 
-    controller.getRawAxis(Constants.RIGHT_X_AXIS)*Constants.TURNING_SCALE), drivetrain));
+    // default case, balances arm without changing position.
+    tower.setDefaultCommand(new RunCommand(() -> {tower.armMoveVoltage(0);},tower));
+    // buttons, move arm forward and backward
+    //set up arm preset positions
+    c_aButton
+      .onTrue(new InstantCommand(() -> tower.setTargetPosition(Constants.Arm.kHomePosition, tower)));
+    c_bButton      
+      .onTrue(new InstantCommand(() -> tower.setTargetPosition(Constants.Arm.kScoringPosition, tower)));
+   c_yButton
+      .onTrue(new InstantCommand(() -> tower.setTargetPosition(Constants.Arm.kIntakePosition, tower)));
+    c_xButton
+      .onTrue(new ParallelCommandGroup(
+        new InstantCommand(() -> tower.setTargetPosition(Constants.Arm.kFeederPosition, tower)),
+         new SequentialCommandGroup(
+          new WaitCommand(0.25), 
+          new InstantCommand(() -> gripper.openConeGripper(), gripper))));
 
-    // Curvature Drive
-   // m_drivetrain.setDefaultCommand(new RunCommand(() -> m_drivetrain.setMotorsCurvature(controller.getRawAxis(Constants.LEFT_AXIS), 
-    //    controller.getRawAxis(Constants.RIGHT_X_AXIS), controller.getRawButton(Constants.LEFT_TRIGGER)), m_drivetrain));
+    //Creates a default command for runing the tower down using the right trigger
+    tower.setDefaultCommand(new RunCommand(
+      () ->
+      tower.runAutomatic()
+      , tower)
+    );
+    new Trigger(() -> 
+      Math.abs(Math.pow(controller2.getRawAxis(3), 2) - Math.pow(controller2.getRawAxis(2), 2)) > Constants.OperatorConstants.kArmManualDeadband
+      ).whileTrue(new RunCommand(
+        () ->
+        tower.runManual((Math.pow(controller2.getRawAxis(3), 2) - Math.pow(controller2.getRawAxis(2), 2)) * Constants.OperatorConstants.kArmManualScale)
+        , tower));
 
-    gripper.setDefaultCommand(new RunCommand(() -> {gripper.setMotors(0);},gripper));
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    //gripper.setDefaultCommand(new RunCommand(() -> {gripper.setMotors(0);},gripper));
 
    //While held this will open the gripper using a run command that executes the mehtod manually
-   lBumper.whileTrue(new RunCommand(() -> {gripper.setMotors(-0.1);}, gripper));
+   //lBumper.whileTrue(new RunCommand(() -> {gripper.setMotors(-0.1);}, gripper));
 
    //While held this will close the gripper using a run command that executes the mehtod manually
-   rBumper.whileTrue(new RunCommand(() -> {gripper.setMotors(0.1);}, gripper));
-   
+   //rBumper.whileTrue(new RunCommand(() -> {gripper.setMotors(0.1);}, gripper));
+
+   defaultAllianceColor();
+
+   //abutton.onTrue(m_blinkin.solidRedCommand());
+   c0_xButton.onTrue(m_blinkin.solidVioletCommand());
+   c0_aButton.onTrue(m_blinkin.solidOrangeCommand());
+   //ybutton.onTrue(m_blinkin.allianceColorCommand());
+
+   //While held this will open the gripper using a run command that executes the mehtod manually
+
+
+    drivetrain.setDefaultCommand(new RunCommand(
+      () -> 
+        drivetrain.driveArcade(
+          MathUtil.applyDeadband(- controller.getRawAxis(1), Constants.OperatorConstants.kDriveDeadband),
+          MathUtil.applyDeadband(controller.getRawAxis(4)*Constants.Drivetrain.kTurningScale, Constants.OperatorConstants.kDriveDeadband))
+  , drivetrain)
+    );
   }
+
+  public double defaultAllianceColor(){
+    boolean isRed = (DriverStation.getAlliance() == Alliance.Red);
+    if (isRed){
+      
+      return-0.35;
+      
+    } else {
+      return 0.87;
+      
+    }
+  }
+  
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -94,6 +212,14 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new CMD_AutoDrive(drivetrain).withTimeout(Constants.AUTO_TIME_SECS);
+    Command chosenAuto = AutoChooser.getSelected();
+    int delay = DelayChooser.getSelected();
+    drivetrain.zeroEncoders();
+    drivetrain.zeroHeading();
+    return new SequentialCommandGroup(new WaitCommand(delay), chosenAuto);
   }
+
+
+   
+
 }
