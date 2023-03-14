49fbe43082fe63474f69dea3b3ebbe77f9453000
Spencer McGrath <Samcgrath2005@gmail.com>
2023-03-14 15:37:12 -0400

    Build things

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2023Season";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 282;
+  public static final String GIT_SHA = "575abad6050e23e92a5e5aff770deb7b5178ee02";
+  public static final String GIT_DATE = "2023-03-13 15:37:39 EDT";
+  public static final String GIT_BRANCH = "Advantage-Kit";
+  public static final String BUILD_DATE = "2023-03-14 12:22:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1678810957810L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
