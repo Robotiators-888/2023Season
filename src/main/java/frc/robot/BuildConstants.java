8e236f14305e343949c61a3ba26a745bcc268bed
Spencer McGrath <Samcgrath2005@gmail.com>
2024-01-27 13:26:41 -0500

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 397;
-  public static final String GIT_SHA = "ca8f1bbcc5dd18513d4cdd289722ac6e1464b944";
-  public static final String GIT_DATE = "2023-04-12 12:06:35 EDT";
+  public static final int GIT_REVISION = 398;
+  public static final String GIT_SHA = "d8a51f5556fc704a41e953df0d0a94adc00e1599";
+  public static final String GIT_DATE = "2024-01-04 15:48:26 EST";
   public static final String GIT_BRANCH = "2024-Drivebase";
-  public static final String BUILD_DATE = "2024-01-04 15:46:41 EST";
-  public static final long BUILD_UNIX_TIME = 1704401201604L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-01-04 15:48:36 EST";
+  public static final long BUILD_UNIX_TIME = 1704401316679L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
